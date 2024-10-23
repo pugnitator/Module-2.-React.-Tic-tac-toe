9ae3b6781a1b542a6c5a6d1a600105723ef46c98
pugnitator <pugnitator2000@gmail.com>
2024-10-23 16:03:08 +0300

    move states to App

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../App.css';
 import { FieldLayout } from './Field';
 
 export function App() {
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [gamer, setGamer] = useState(true); 	//true - крестик, false - это нолик
   return (
-      <FieldLayout></FieldLayout>
+      <FieldLayout props = {{'cells': cells, 'setCells': setCells, 'gamer': gamer, 'setGamer': setGamer}}></FieldLayout>
   );
 }
--- my-app/src/components/Field.jsx
+++ my-app/src/components/Field.jsx
@@ -1,28 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 
-export function FieldLayout() {
-	const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
-    //true - крестик, false - это нолик
-    const [gamer, setGamer] = useState(true);
+export function FieldLayout(props) {
+    const {cells, setCells, gamer, setGamer} = props.props;
+    console.log(props.props)
 
 	const Container = styled.div`
 		display: grid;
 		grid-template: repeat(3, 1fr) / repeat(3, 1fr);
-        box-sizing: border-box;
-        margin: auto;
-        height: 40vw;
-        width: 40vw;
+		box-sizing: border-box;
+		margin: auto;
+		height: 40vw;
+		width: 40vw;
 	`;
 
 	function onClickButton(e) {
+		// TODO: Нельзя менять значение, если оно не undefiend + красная подсветка
 		const cellId = e.target.dataset.id;
-        console.log(e);
-        const newCells = cells;
-        newCells[cellId] = gamer ? 'X' : 'O';
-        console.log(newCells);
-        setCells(newCells);
-        setGamer(!gamer)
+		console.log(e);
+		const newCells = cells;
+		if (!newCells[cellId]) {
+			newCells[cellId] = gamer ? "X" : "O";
+			setCells(newCells);
+			setGamer(!gamer);
+		} else {
+            console.log('nelzya')
+        }
 	}
 
 	return (
@@ -34,15 +37,17 @@ export function FieldLayout() {
 	);
 }
 
-function Cell({index, value}) {
-	const CellButton = styled.button.attrs(props => ({'data-id': props['data-id']}))`
+function Cell({ index, value }) {
+	const CellButton = styled.button.attrs((props) => ({
+		"data-id": props["data-id"],
+	}))`
 		background-color: #f4faff;
-		border: 1px solid #2E9AFF;
-        &:hover {
-            background-color: #E6EBF0;
-            cursor: pointer;
-        };
-        font-size: 50px;
+		border: 1px solid #2e9aff;
+		&:hover {
+			background-color: #e6ebf0;
+			cursor: pointer;
+		}
+		font-size: 50px;
 	`;
 	return <CellButton data-id={index}>{value}</CellButton>;
 }
